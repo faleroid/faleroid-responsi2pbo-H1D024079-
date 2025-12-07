19f8fd203c8c515bfeffb779c9ca7e2752421f35
naufal <naufalsatrioputra@gmail.com>
2025-12-07 14:03:25 +0700

    fix: pert4 final

--- pert4/UjiCustomer.java
+++ pert4/UjiCustomer.java
@@ -1,14 +1,22 @@
 public class UjiCustomer {
     public static void main(String[] args) {
+        // TODO: Tampilkan judul data pelanggan
+        System.out.println("=== DATA CUSTOMER BELANJAKU ===");
 
         // TODO: Buat object Customer
-
-        // TODO: Buat object Member
-
-        // TODO: Tampilkan judul data pelanggan
+        Customer c = new Customer("Budi Santoso", "CST-001", 500000);
 
         // TODO: Tampilkan info untuk Customer biasa
+        System.out.println("Status: Customer Biasa");
+        c.tampilkanInfo();
+
+        System.out.println(); // Spasi
+
+        // TODO: Buat object Member
+        Member m = new Member("Siti Aminah", "MBR-110", 1250000, 240, "Gold");
 
         // TODO: Tampilkan info untuk pelanggan Member
+        System.out.println("Status: Member");
+        m.tampilkanInfo();
     }
 }
